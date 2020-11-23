486e1627e7ec15825b642dc706e689266c06826d
SayMouz <faouz5307@gmail.com>
2020-11-23 15:23:00 +0100

    pull

--- app/src/main/java/deliverif/app/view/PrimaryController.java
+++ app/src/main/java/deliverif/app/view/PrimaryController.java
@@ -2,8 +2,12 @@ package deliverif.app.view;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
+
+    @FXML
+    private Button primaryButton;
 
     @FXML
     private void switchToSecondary() throws IOException {
