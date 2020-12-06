0c5216e82539d01b9eb7c87c272e58ca8eeadd85
MKL-IT <lele.matok@gmail.com>
2020-12-06 11:52:48 +0100

    one list

--- app/src/main/java/deliverif/app/controller/MenuPageController.java
+++ app/src/main/java/deliverif/app/controller/MenuPageController.java
@@ -91,9 +91,6 @@ public class MenuPageController implements Observer {
     @FXML
     private ListView<Text> requestList;
 
-    @FXML
-    private ListView<Text> pathList;
-
     private final XmlReader xmlReader = new XmlReader();
 
     private Map map;
@@ -108,7 +105,7 @@ public class MenuPageController implements Observer {
 
     private GraphProcessor graphProcessor;
 
-    private Tour tour;
+    private Tour tour = null;
 
     private String[] selectedEdges = null;
 
@@ -140,9 +137,9 @@ public class MenuPageController implements Observer {
             for (String id : this.graphEdges) {
                 if (element.getId().equals(id)) {
                     System.out.println(id + " is on the path");
-                    for (Text t : this.pathList.getItems()) {
+                    for (Text t : this.requestList.getItems()) {
                         if (t.getId().contains(id)) {
-                            this.pathList.getSelectionModel().select(t);
+                            this.requestList.getSelectionModel().select(t);
                             String[] ids = t.getId().split("#");
                             String numString = t.getText().substring(1, 2);
                             int num = Integer.parseInt(numString);
@@ -177,6 +174,9 @@ public class MenuPageController implements Observer {
             return;
         }
         if (element.getSelectorType() != Selector.Type.SPRITE) {
+            return;
+        }  
+        if (this.tour != null) {
             return;
         }
 
@@ -248,14 +248,21 @@ public class MenuPageController implements Observer {
         renderTour();
     }
     public void renderTour() {
+        
         for (String edgeId : graphEdges) {
             resetEdge(edgeId);
         }
         graphEdges.clear();
+        
+        this.requestList.getItems().clear();
+        
         Text txt = null;
         int cpt = 1;
+        SimpleDateFormat dtf = new SimpleDateFormat("HH:mm:ss"); 
+        int[] rgb = randomColorSprite();
+        
         for (Path p : tour.getPaths()) {
-            txt = new Text("[" + cpt + "]");
+            
             String id = "";
             for (Segment s : p.getSegments()) {
                 String originId = s.getOrigin().getId().toString();
@@ -280,8 +287,20 @@ public class MenuPageController implements Observer {
                     }
                 }
             }
+            Long departId = p.getDeparture().getId();
+            String typePoint = tour.getPr().researchTypeIntersection(departId);
+            
+            if(typePoint != ""){
+                txt = new Text("[" + cpt + "] "+typePoint+" from "+ dtf.format(p.getDepatureTime())
+                                + " to " + dtf.format(p.getArrivalTime()) + " "+ p.getLength()/1000+ "km");
+            }else{
+                txt = new Text("[" + cpt + "] Depot from "+ dtf.format(p.getDepatureTime())
+                                + " to " + dtf.format(p.getArrivalTime()) + " "+ p.getLength()/1000+ "km");
+            }
+            
+            txt.setFill(Color.rgb(rgb[0], rgb[1], rgb[2]));
             txt.setId(id);
-            this.pathList.getItems().add(txt);
+            this.requestList.getItems().add(txt);
             cpt++;
         }
         
@@ -289,7 +308,7 @@ public class MenuPageController implements Observer {
         int time = this.tour.getTotalDuration();
         
         System.out.println("getDuration/"+time);
-        distance = distance / 100;
+        distance = distance / 1000;
         float duration = time/ 60;
         System.out.println("Duration/"+duration);
         int hours = (int)duration;
@@ -300,7 +319,6 @@ public class MenuPageController implements Observer {
         Date departure = this.tour.getDepartureTime();
         Date arrival = this.tour.getArrivalTime();
         
-        SimpleDateFormat dtf = new SimpleDateFormat("HH:mm:ss");   
         this.infosTextTour1.setText("TOUR = " + distance + " km / " + hours +"h"+ mins + "min");
         this.infosTextTour2.setText("from " + dtf.format(departure) + " to " + dtf.format(arrival));
 
@@ -349,27 +367,25 @@ public class MenuPageController implements Observer {
         this.planningRequest = null;
         this.tour = null;
         this.requestList.getItems().clear();
-        this.pathList.getItems().clear();
         this.longitudeText.setText("Longitude = ");
         this.latitudeText.setText("Latitude = ");
         this.infosText.setText("");
-
+        this.infosTextTour1.setText("Tour infos = ");
+        this.infosTextTour2.setText("");
     }
 
     @FXML
     public void requestListClick(MouseEvent arg0) {
-        System.out.println("clicked on " + requestList.getSelectionModel().getSelectedItem().getText());
-
-        String spriteId = requestList.getSelectionModel().getSelectedItem().getId();
-        this.currentState.selectNode(spriteId);
-
-    }
-
-    @FXML
-    public void pathListClick(MouseEvent arg0) {
-        System.out.println("clicked on " + this.pathList.getSelectionModel().getSelectedItem().getText());
-        int num = Integer.parseInt(this.pathList.getSelectionModel().getSelectedItem().getText().substring(1, 2));
-        this.setSelectedPath(num);
+        
+        if (this.tour == null){
+            System.out.println("clicked on " + requestList.getSelectionModel().getSelectedItem().getText());
+            String spriteId = requestList.getSelectionModel().getSelectedItem().getId();
+            this.currentState.selectNode(spriteId);
+        }else{
+            System.out.println("clicked on " + this.requestList.getSelectionModel().getSelectedItem().getText());
+            int num = Integer.parseInt(this.requestList.getSelectionModel().getSelectedItem().getText().substring(1, 2));
+            this.setSelectedPath(num);
+        }
     }
 
     private void setSelectedPath(int num) {
