72f98a7d0497d9693e0bfb6437bd6dda6a927656
Polo <pablo_mo@orange.fr>
2020-12-08 12:03:28 +0100

    WIP better tsp

--- app/src/main/java/deliverif/app/controller/tsp/TSP1.java
+++ app/src/main/java/deliverif/app/controller/tsp/TSP1.java
@@ -14,28 +14,30 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
-
 /**
  *
  * @author zakaria
  */
 public class TSP1 extends TemplateTSP {
+
     public float mindis;
-    public HashMap<String,Float> costs = new HashMap<>();
-    
-    @Override
-    protected float bound(Vertex currentVertex, ArrayList<Vertex> unvisited) {
+    public HashMap<String, Float> costs = new HashMap<>();
+
+    protected float bound2(Vertex currentVertex, ArrayList<Vertex> unvisited) {
         float total1 = Float.MAX_VALUE;
         float total2 = 0;
-        for (int i =0; i<unvisited.size(); i++){
-            float current = costs.get(currentVertex.getId()+"-"+unvisited.get(i).getId());
-            if (current < total1) total1 = current;   
-            float current2 = unvisited.get(i).getId();
-            float shortest = costs.get(current2+"-"+"0");
-            for (int j=0; j<unvisited.size(); j++){
-                if (i != j){
-                    float curr = costs.get(current2+"-"+unvisited.get(j).getId());
-                    if(curr < shortest){
+        for (int i = 0; i < unvisited.size(); i++) {
+            float current = costs.get(currentVertex.getId() + "-" + unvisited.get(i).getId());
+            if (current < total1) {
+                total1 = current;
+            }
+            Long current2 = unvisited.get(i).getId();
+            System.out.println("Get: " + current2 + "-" + "0");
+            float shortest = costs.get(current2 + "-" + "0");
+            for (int j = 0; j < unvisited.size(); j++) {
+                if (i != j) {
+                    float curr = costs.get(current2 + "-" + unvisited.get(j).getId());
+                    if (curr < shortest) {
                         shortest = curr;
                     }
                 }
@@ -44,30 +46,34 @@ public class TSP1 extends TemplateTSP {
         }
         return total1 + total2;
     }
-    
-    protected float bound2(Vertex currentVertex, ArrayList<Vertex> unvisited) {  
-        return mindis * (unvisited.size()+1) ;
+
+    @Override
+    protected float bound(Vertex currentVertex, ArrayList<Vertex> unvisited) {
+        return mindis * (unvisited.size() + 1);
     }
-    
+
     protected void onInit(Graph g, Vertex start) {
-        for (Vertex v : g.getVertexMap().values()){
+        for (Vertex v : g.getVertexMap().values()) {
             for (Edge e : v.getAdj()) {
                 if (e.dest.getId() == start.getId()) {
-                    costs.put(v.getId()+"-"+"0", e.cost);
-                }else{
-                    costs.put(v.getId()+"-"+e.dest.getId(), e.cost);
+                    System.out.println("Add: " + v.getId() + "-" + "0");
+                    costs.put(v.getId() + "-" + "0", e.cost);
+                } else {
+                    System.out.println("Add: " + v.getId() + "-" + e.dest.getId());
+                    costs.put(v.getId() + "-" + e.dest.getId(), e.cost);
                 }
             }
         }
     }
-    
+
     @Override
     protected Iterator<Vertex> iterator(Vertex currentVertex, Collection<Vertex> unvisited, Graph g) {
-            return new SeqIter(unvisited, currentVertex, g);
+        return new SeqIter(unvisited, currentVertex, g);
     }
-    
+
     @Override
-    public void searchSolution(int timeLimit, Graph g, Vertex start, List<Long> ordre){
+    public void searchSolution(int timeLimit, Graph g, Vertex start, List<Long> ordre) {
+        // onInit(g, start);
         mindis = g.getMinDis();
         super.searchSolution(timeLimit, g, start, ordre);
     }
